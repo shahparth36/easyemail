9e2586f47ec505d31955b9ace334c44986b1fcbb
shahparth36 <parthshah1936@gmail.com>
2021-03-10 15:40:46 +0530

    Added email service

--- email/index.js
+++ email/index.js
@@ -1,7 +1,23 @@
-function greet() {
-    console.log("good morning!");
-}
+const nodemailer = require("nodemailer");
 
+async function sendEmail(email, subjectOfEmail, textToBeSentInEmail) {
+    var transporter = nodemailer.createTransport({
+        service: "Gmail",
+        auth: {
+            user: 'parthshah1936@gmail.com',
+            pass: 'adgzcbqet19',
+        },
+    });
+
+    transporter.sendMail({
+        from: 'smartexamportal@gmail.com',
+        to: email,
+        subject: subjectOfEmail,
+        text: textToBeSentInEmail
+    });
+
+    transporter.close();
+}
 module.exports = {
-    greet
+    sendEmail
 }
